9e4249d2c9661252b22c0fa81cfb84cc3d626755
Farok Hossain <farokafs7@gmail.com>
2024-06-29 16:36:05 +0600

    image added

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import myImage from "../../../public/image/myImage.jpg";
+
 const GalleryPage = () => {
   return (
     <div>
       <h1 className="text-center text-4xl">Image Optimizations</h1>
       <h2>Regular Image Tag</h2>
+      <img src={myImage} alt="image" className="mx-auto" />
+      <Image src={myImage} alt="image" width={500} className="mx-auto" />
+      <Image
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fcat%2F&psig=AOvVaw1KkvwiByhsFSwtIuxUqGZ9&ust=1719742740456000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLC9iYzLgIcDFQAAAAAdAAAAABAE"
+        alt="image"
+        width={500}
+        height={400}
+        className="mx-auto mt-4"
+      />
     </div>
   );
 };
