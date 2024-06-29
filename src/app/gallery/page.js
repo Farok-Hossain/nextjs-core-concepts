387d6f39e0c81e00d09ee55294de585b300cdd01
Farok Hossain <farokafs7@gmail.com>
2024-06-29 16:07:19 +0600

    gallery component added

---
+++ src/app/gallery/page.js
@@ -0,0 +1,10 @@
+const GalleryPage = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl">Image Optimizations</h1>
+      <h2>Regular Image Tag</h2>
+    </div>
+  );
+};
+
+export default GalleryPage;
